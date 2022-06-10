7dce5d0536f3ad94d36cba7c454742c2476f36c3
Edwin Nord <edwin.nord09@gmail.com>
2022-06-10 13:19:30 -0400

    Js Update

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let form = document.getElementById('form');
 let email = document.getElementById('email');
 let submit = document.getElementById('submit-btn');
 let error = document.querySelector('.error')
-let loading = document.querySelector('.loading')
+let success = document.querySelector('.success')
 
 
 
@@ -10,20 +10,20 @@ form.addEventListener('submit', (e) => {
     const regEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     if(email.value === '' || email.value === null) {
-        
         error.style.display = 'block'
+        success.style.display = 'none'
         
         e.preventDefault()
-    } if(regEmail.value = false) {
-        
-        // error.style.display = 'block'
+    }else if(regEmail.value = false) {
+        error.style.display = 'block'
+        success.style.display = 'none'
         e.preventDefault()
-
     } 
+
     
     else {
-        document.querySelector('.loader').style.display = 'block';
-        setTimeout(loading, 6000);
+        success.style.display = 'block';
+        setTimeout(success, 6000);
     }
         
 })
